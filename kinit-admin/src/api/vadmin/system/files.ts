b206e38d467381e8ef7a136a7bd57351fd8f77d4
ktianc <ktianc2000@163.com>
2023-08-08 11:16:26 +0800

    editor 富文本编辑器上传图片，视频功能补充更新

--- kinit-admin/src/api/vadmin/system/files.ts
+++ kinit-admin/src/api/vadmin/system/files.ts
@@ -1,8 +1,27 @@
 import request from '@/config/axios'
 
-export const addFilesListApi = (data: any): Promise<IResponse> => {
+// 上传图片到阿里云OSS
+export const uploadImageToOSS = (data: any): Promise<IResponse> => {
   return request.post({
-    url: `/vadmin/system/files`,
+    url: `/vadmin/system/upload/image/to/oss`,
+    headersType: 'multipart/form-data',
+    data
+  })
+}
+
+// 上传图片到阿里云OSS
+export const uploadVideoToOSS = (data: any): Promise<IResponse> => {
+  return request.post({
+    url: `/vadmin/system/upload/video/to/oss`,
+    headersType: 'multipart/form-data',
+    data
+  })
+}
+
+// 上传图片到阿里云OSS
+export const uploadFileToOSS = (data: any): Promise<IResponse> => {
+  return request.post({
+    url: `/vadmin/system/upload/file/to/oss`,
     headersType: 'multipart/form-data',
     data
   })
