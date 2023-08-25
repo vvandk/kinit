1ee46b24f77975ac807600e2e393027ba9db02fa
ktianc <ktianc2000@163.com>
2023-08-25 17:11:58 +0800

    修复映射数据库表失败问题

--- kinit-api/alembic/env.py
+++ kinit-api/alembic/env.py
@@ -8,7 +8,7 @@ from alembic import context
 import os
 import sys
 
-from core.database import Model
+from core.database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -37,22 +37,16 @@ sys.path.append(BASE_DIR)
 from apps.vadmin.auth.models import *
 from apps.vadmin.system.models import *
 from apps.vadmin.record.models import *
+from apps.vadmin.help.models import *
+from apps.vadmin.resource.models import *
 
 # 修改配置中的参数
-target_metadata = Model.metadata
+target_metadata = Base.metadata
 
 
 def run_migrations_offline():
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
+    """
+    以“脱机”模式运行迁移。
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
@@ -69,11 +63,8 @@ def run_migrations_offline():
 
 
 def run_migrations_online():
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
+    """
+    以“在线”模式运行迁移。
     """
     connectable = engine_from_config(
         config.get_section(config.config_ini_section),
@@ -91,6 +82,8 @@ def run_migrations_online():
 
 
 if context.is_offline_mode():
+    print("offline")
     run_migrations_offline()
 else:
+    print("online")
     run_migrations_online()
