44fd3adf5762606d3f1de25b25daa909ab121b20
luck_baby <ranyong1209@gmail.com>
2024-03-12 17:47:02 +0800

    ✨ Feat(Adding pytest tests): 新增单元测试

--- kinit-api/tests/vadmin/system_certification/test_auth.py
+++ kinit-api/tests/vadmin/system_certification/test_auth.py
@@ -2,22 +2,49 @@
 # -*- coding: utf-8 -*-
 # @Time     : 2024/3/11 17:52
 # @Author   : 冉勇
-# @File     : test_auth.py
+# @File     : test_autaccess_token.py
 # @Software : PyCharm
-# @Desc     : 系统认证
+# @Desc     : 权限管理
+import pytest
 from fastapi.testclient import TestClient
-from apps.vadmin.auth.utils.login import app
+from apps.vadmin.auth.views import app
+from fastapi import status
 
-client = TestClient(app)
+# 模拟认证
+app.dependency_overrides = {}
+
+# 定义一个假的token
+test_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxNTAyMDIyMTAxMCIsInBhc3N3b3JkIjoiJDJiJDEyJENlN2VTVUtJSWw4RE1LZUR5Tkh5ci5EcDRhZXNRQ003MFJlUGlnUlZFbnkxRXFsMzFSMENxIiwiZXhwIjoxNzEwMzExNjMxfQ.hOBOHLt4e2uTYAGyVcUKKr9X0w84f6Lx8--fwHoGkNo"
 
 
-def test_login():
-    login_data = {
-        "username ": "15020221010",
-        "password": "kinit2022"
+def test_get_user(test_client):
+    headers = {"Authorization": f"Bearer {test_token}"}
+    response = test_client.get("/users", headers=headers)
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_create_user(test_client):
+    headers = {"Authorization": f"Bearer {test_token}"}
+    # todo: 写一个随机手机号hook或者一个硬删除接口
+    data = {
+        "name": "test001",
+        "telephone": "13333333333",
+        "email": "test@example.com",
+        "nickname": "测试001",
+        "avatar": "",
+        "is_active": "true",
+        "is_staff": "true",
+        "gender": "0",
+        "is_wx_server_openid": "false",
+        "role_ids": [],
+        "dept_ids": [],
+        "password": ""
     }
-    response = client.post("/auth/api/login", json=login_data)
-    assert response.status_code == 200
-    assert "access_token" in response.json()
-    # 或者是否包含了某种形式的错误消息（取决于你的应用逻辑）
-    # assert response.json() == {"detail": "Invalid credentials"}
+    response = test_client.post("/users", headers=headers, json=data)
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.fixture
+def test_client():
+    client = TestClient(app)
+    return client
--- kinit-api/tests/vadmin/test_login.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time     : 2024/3/11 17:52
-# @Author   : 冉勇
-# @File     : test_login.py
-# @Software : PyCharm
-# @Desc     :
