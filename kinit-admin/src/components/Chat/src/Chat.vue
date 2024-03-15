53a694be367e0b30a1070e9a72476cf01be8db90
ktianc <2445667550@qq.com>
2024-03-15 17:39:17 +0800

    更新1

--- kinit-admin/src/components/Chat/src/Chat.vue
+++ kinit-admin/src/components/Chat/src/Chat.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { Icon } from '@/components/Icon'
 import { propTypes } from '@/utils/propTypes'
-import { Dialog } from '@/components/Dialog'
+import { ElDialog, ElContainer, ElAside, ElHeader, ElMain } from 'element-plus'
 import { ref } from 'vue'
 import { useDesign } from '@/hooks/web/useDesign'
 
@@ -14,7 +14,6 @@ defineProps({
 })
 
 const dialogVisible = ref(false)
-const dialogTitle = ref('聊天室')
 
 const toggleFullscreen = () => {
   dialogVisible.value = true
@@ -25,6 +24,35 @@ const toggleFullscreen = () => {
   <div :class="prefixCls" @click="toggleFullscreen">
     <Icon :size="18" icon="bi:chat-dots" :color="color" />
 
-    <Dialog v-model="dialogVisible" :title="dialogTitle" :height="650"><div></div> </Dialog>
+    <ElDialog
+      v-model="dialogVisible"
+      class="chat-dialog !p-[0px]"
+      :fullscreen="false"
+      destroy-on-close
+      lock-scroll
+      draggable
+      top="8vh"
+      width="839px"
+      :close-on-click-modal="true"
+      :show-close="false"
+    >
+      <div class="flex justify-start h-[708px]">
+        <div class="border-r-solid border-r-1 border-gray-300 w-[200px]"> Aside </div>
+        <div>
+          <div class="h-[200px]">Header</div>
+          <div>Main</div>
+        </div>
+      </div>
+    </ElDialog>
   </div>
 </template>
+
+<style lang="less">
+.chat-dialog .el-dialog__header {
+  display: none;
+}
+
+.chat-dialog .el-dialog__body {
+  padding: 0px !important;
+}
+</style>
