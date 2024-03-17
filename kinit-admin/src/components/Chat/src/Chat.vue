332e728ca3a75163241af1bd49aad3c21c23c46e
ktianc <2445667550@qq.com>
2024-03-17 04:14:35 +0000

    Revert "更新1"
    This reverts commit 53a694be367e0b30a1070e9a72476cf01be8db90.

--- kinit-admin/src/components/Chat/src/Chat.vue
+++ kinit-admin/src/components/Chat/src/Chat.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { Icon } from '@/components/Icon'
 import { propTypes } from '@/utils/propTypes'
-import { ElDialog, ElContainer, ElAside, ElHeader, ElMain } from 'element-plus'
+import { Dialog } from '@/components/Dialog'
 import { ref } from 'vue'
 import { useDesign } from '@/hooks/web/useDesign'
 
@@ -14,6 +14,7 @@ defineProps({
 })
 
 const dialogVisible = ref(false)
+const dialogTitle = ref('聊天室')
 
 const toggleFullscreen = () => {
   dialogVisible.value = true
@@ -24,35 +25,6 @@ const toggleFullscreen = () => {
   <div :class="prefixCls" @click="toggleFullscreen">
     <Icon :size="18" icon="bi:chat-dots" :color="color" />
 
-    <ElDialog
-      v-model="dialogVisible"
-      class="chat-dialog !p-[0px]"
-      :fullscreen="false"
-      destroy-on-close
-      lock-scroll
-      draggable
-      top="8vh"
-      width="839px"
-      :close-on-click-modal="true"
-      :show-close="false"
-    >
-      <div class="flex justify-start h-[708px]">
-        <div class="border-r-solid border-r-1 border-gray-300 w-[200px]"> Aside </div>
-        <div>
-          <div class="h-[200px]">Header</div>
-          <div>Main</div>
-        </div>
-      </div>
-    </ElDialog>
+    <Dialog v-model="dialogVisible" :title="dialogTitle" :height="650"><div></div> </Dialog>
   </div>
 </template>
-
-<style lang="less">
-.chat-dialog .el-dialog__header {
-  display: none;
-}
-
-.chat-dialog .el-dialog__body {
-  padding: 0px !important;
-}
-</style>
